9d6db5306047ba0d60f44818895247163db2825e
BigBenLau <benlau413@gmail.com>
2019-05-29 03:10:41 +0800

    build distance > 8, finish

--- main/main.js
+++ main/main.js
@@ -5,7 +5,8 @@ module.exports = function main(inputs) {
 
 function dist(distance){
 	if(distance <= 2){return 6;}
-	else if(distance > 2 && distance <= 8){return 6 + 0.8*(distance - 2)}
+	else if(distance > 2 && distance <= 8){return 6 + 0.8*(distance - 2);}
+	else if(distance > 8){return 6 + 4.8 + 0.8*(1 + 0.5)*(distance - 8);}
 }
 
 function park(time){
